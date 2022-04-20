b4cfed36660a2152ab477f94c17731ba6b1a2115
laurent-tupin <Mv2nxr33!>
2022-04-20 15:09:39 +0800

    Test + Documentation on FTP Test, empty file

---
+++ test/test_nutFtp.py
@@ -0,0 +1,17 @@
+import pytest
+try:
+    import nutFtp as ftp
+except:
+    print('Online Test...')
+    from pyNut import nutFtp as ftp
+
+
+#=============================================================================
+# UNIT TEST
+#=============================================================================
+
+
+
+
+
+
